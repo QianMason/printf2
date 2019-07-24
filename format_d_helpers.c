ba438ae6540256e0c77d56d106ac858b87fee532
Mason Qian <mqian@e1z3r1p24.42.us.org>
2019-07-23 18:23:42 -0700

    kind of finished format_d_right but holy fuck its 200 lines, gonna need to trim and finish up the logic cause i feel like it doesnt handle all the cases oh well

--- format_d_helpers.c
+++ format_d_helpers.c
@@ -6,7 +6,7 @@
 /*   By: mqian <mqian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 21:36:29 by Thunderpurt       #+#    #+#             */
-/*   Updated: 2019/07/23 16:22:15 by mqian            ###   ########.fr       */
+/*   Updated: 2019/07/23 18:22:54 by mqian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int		format_d_left(int flags[], int argument) //left align
 		}
 		if (flags[7] > arg_len)
 		{
-			while (count < flags[6] - arg_len)
+			while (count < flags[7] - arg_len)
 				count += write_and_increment('0');
 			ft_putnbr(argument);
 			count += arg_len;
@@ -83,24 +83,191 @@ int		format_d_right(int flags[], int argument)
 	arg_len = get_int_len(argument);
 	if (flags[0] == 1) // prepend a + or - based on argument sign
 	{
-		if (flags[6] > arg_len)
+		if (flags[5] > flags[7]) //minw greater than precision
 		{
-			while (count < flags[6] - arg_len)
+			if (flags[7] > arg_len) //precision is greater than arg_len
 			{
-				write(1, ' ', 1);
-				count++;
+				if (argument == 0)
+				{
+					while (count < flags[5] - flags[7])
+						count += write_and_increment(' ');
+				}
+				else if (argument < 0)
+				{
+					while (count < flags[5] - flags[7] - 1)
+						count += write_and_increment(' ');
+					count += write_and_increment('-');
+					argument *= -1;
+				}
+				else if (argument > 0)
+				{
+					while (count < flags[5] - flags[7] - 1)
+						count += write_and_increment(' ');
+					count += write_and_increment('+');
+				}
+				while (count < flags[5] - arg_len)
+					count += write_and_increment('0');
+				ft_putnbr(argument);
+				count += arg_len;
 			}
-			if (argument < 0)
+			else //flags[7] <= arg_len, precision is less than or equal to arg_len so ignore
 			{
-				write(1, "-", 1);
-				count++;
+				if (arg_len > flags[5])
+				{
+					if (argument == 0)
+					{
+						ft_putnbr(argument);
+						count++;
+					}
+					else if (argument < 0)
+					{
+						count += write_and_increment('-');
+						argument *= -1;
+					}
+					else 
+						count += write_and_increment('+');
+					ft_putnbr(argument);
+					count += arg_len;
+				}
+				else //arg_len <= minw (flags[5]) precision ignored
+				{
+					if (flags[3] == 1)
+					{
+						if (argument == 0)
+							count += write_and_increment(' ');
+						else if (argument < 0)
+						{
+							count += write_and_increment('-');
+							argument *= -1;
+						}
+						else
+							count += write_and_increment('+');
+						while (count < flags[5] - arg_len)
+							count += write_and_increment('0');
+					}
+					else
+					{
+						while (count < flags[5] - arg_len - 1)
+							count += write_and_increment(' ');
+						if (argument == 0)
+							count += write_and_increment(' ');
+						else if (argument < 0)
+						{
+							count += write_and_increment('-');
+							argument *= -1;
+						}
+						else
+							count += write_and_increment('+');
+					}
+					ft_putnbr(argument);
+					count += arg_len;
+				}
+			}
+		}
+		else //minw flags[5] <= precision flags[7]
+		{
+			if (argument > 0)
+			{
+				count += write_and_increment('+');
+				flags[7]++;
+			}
+			else if (argument < 0)
+			{
+				count += write_and_increment('-');
+				flags[7]++;
 				argument *= -1;
 			}
-			else if (argument > 0)
+			if (flags[7] - 1 > arg_len) //precision greater than arg_len, need to pad with 0s
 			{
-				write(1, "+", 1);
-				count++;
+				while (count < flags[7] - arg_len)
+					count += write_and_increment('0');
+				ft_putnbr(argument);
+				count += arg_len;
+			}
+			else //arg_len >= flags[7], arg_len >precision >= flags[5], so print prepension and then just print the number since its right justified.
+			{
+				ft_putnbr(argument);
+				count += arg_len;
 			}
 		}
 	}
+	else //no +/- prepensions
+	{
+		if (flags[5] > flags[7]) //minw greater than precision
+		{
+			if (flags[7] > arg_len)
+			{
+				while (count < flags[5] - flags[7] - 1)
+				{
+					if (flags[3] == 1)
+						count += write_and_increment('0');
+					else
+						count += write_and_increment(' ');
+				}
+				if (argument < 0)
+				{
+					count += write_and_increment('-');
+					argument *= -1;
+				}
+				while (count < flags[5] - arg_len) //write zeros til the argument
+					count += write_and_increment('0');
+				ft_putnbr(argument);
+				count += arg_len;
+			}
+			else // arg_len >= precision (flags[7])
+			{
+				if (arg_len > flags[5])
+				{
+					if (argument < 0)
+					{
+						count += write_and_increment('-');
+						argument *= -1;
+					}
+					ft_putnbr(argument);
+					count += arg_len;
+				}
+				else // arg_len <= minw (flags[5]) precision ignored
+				{
+					while (count < flags[5] - arg_len - 1)
+					{
+						if (flags[3] == 1)
+							count += write_and_increment('0');
+						else
+							count += write_and_increment(' ');
+					}
+					if (argument < 0)
+					{
+						count += write_and_increment('-');
+						argument *= -1;
+					}
+					else
+						count += write_and_increment(' ');
+					ft_putnbr(argument);
+					count += arg_len;
+				}
+			}
+		}
+		else // minw less than precision (flags[5] <= flags[7]) essentially left justified
+		{
+			if (argument < 0)
+			{
+				count += write_and_increment('-');
+				flags[7]++;
+				argument *= -1;
+			}
+			if (flags[7] - 1 > arg_len) //precision greater than arg_len, need to pad with 0s
+			{
+				while (count < flags[7] - arg_len)
+					count += write_and_increment('0');
+				ft_putnbr(argument);
+				count += arg_len;
+			}
+			else //arg_len >= flags[7], arg_len >precision >= flags[5], so print prepension and then just print the number since its right justified.
+			{
+				ft_putnbr(argument);
+				count += arg_len;
+			}
+		}
+	}
+	return (count);
 }
